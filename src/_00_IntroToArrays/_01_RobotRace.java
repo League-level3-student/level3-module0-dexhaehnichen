c84df437dcef1660c10123ab53bbae9866b2d4c1
League Student <league@iMac.attlocal.net>
2019-05-27 18:25:35 -0700

    RobotRace(IP)

--- src/_00_IntroToArrays/_01_RobotRace.java
+++ src/_00_IntroToArrays/_01_RobotRace.java
@@ -8,23 +8,33 @@ import org.jointheleague.graphical.robot.Robot;
 
 public class _01_RobotRace {
 	//1. make a main method
-	
+	public static void main(String[] args) {
 		//2. create an array of 5 robots.
-
+Robot[] robots = new Robot[5];
 		//3. use a for loop to initialize the robots.
-
+for(int i = 0; i < 5; i++) {
+	robots[i] = new Robot();
+}
 			//4. make each robot start at the bottom of the screen, side by side, facing up
-	
+for(int i = 1; i < 5; i++) {
+	robots[i].setX(100*i);
+	robots[i].setY(500);
+}	
 		//5. use another for loop to iterate through the array and make each robot move 
 	    //   a random amount less than 50.
-    	
-		//6. use a while loop to repeat step 5 until a robot has reached the top of the screen.
 
+		//6. use a while loop to repeat step 5
+		//until a robot has reached the top of the screen.
+while(robots[0].getY() <= 100 || robots[1].getY() <= 100 || robots[2].getY() <= 100 || robots[3].getY() <= 100 || robots[4].getY() <= 100) {
+	for(int i = 1; i < 5; i++) {
+	//	robots[i].move(50);
+	}
+}
 		//7. declare that robot the winner and throw it a party!
     	
 		//8. try different races with different amounts of robots.
     	
 	    //9. make the robots race around a circular track.
-
+	}
 	
 }
