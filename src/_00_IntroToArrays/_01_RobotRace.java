6926c35c5223e179bd32762a25a06f6783508307
League Student <league@iMac.attlocal.net>
2019-07-08 18:24:43 -0700

    RobotRace

--- src/_00_IntroToArrays/_01_RobotRace.java
+++ src/_00_IntroToArrays/_01_RobotRace.java
@@ -11,13 +11,13 @@ public class _01_RobotRace {
 	// 1. make a main method
 	public static void main(String[] args) {
 		// 2. create an array of 5 robots.
-		Robot[] robots = new Robot[5];
+		Robot[] robots = new Robot[7];
 		// 3. use a for loop to initialize the robots.
-		for (int i = 0; i < 5; i++) {
+		for (int i = 0; i < robots.length; i++) {
 			robots[i] = new Robot();
 		}
 		// 4. make each robot start at the bottom of the screen, side by side, facing up
-		for (int i = 0; i < 5; i++) {
+		for (int i = 0; i < robots.length; i++) {
 			robots[i].setX(100 * (i + 1));
 			robots[i].setY(500);
 		}
@@ -27,23 +27,33 @@ public class _01_RobotRace {
 		// 6. use a while loop to repeat step 5
 		// until a robot has reached the top of the screen.
 		Random r = new Random();
-		while (robots[0].getY() < 0 && robots[1].getY() < 0 && robots[2].getY() < 0 && robots[3].getY() < 0
-				|| robots[4].getY() > 0) {
-			for (int i = 0; i < 5; i++) {
+		/*
+		 * while (robots[0].getY() < 0 && robots[1].getY() < 0 && robots[2].getY() < 0
+		 * && robots[3].getY() < 0 || robots[4].getY() < 0) { for (int i = 0; i < 5;
+		 * i++) {  } }
+		 */
+		boolean isRobotsFinished = false;
+		while(!isRobotsFinished) {
+			for(int i = 0; i < robots.length; i++) {
 				robots[i].move(r.nextInt(51));
+				if(robots[i].getY()<0) {
+					isRobotsFinished = true;
+					break;
+				}
 			}
 		}
 		// 7. declare that robot the winner and throw it a party!
-for(int i = 0; i<robots.length-1; i++) {
-	if(robots[i].getY()<0) {
-		robots[i].setY(500);
-		robots[i].setX(500);
-		robots[i].sparkle();
-	}
-}
+for (int l = 0; l < robots.length - 1; l++) {
+					if (robots[l].getY() < 0) {
+						robots[l].setY(500);
+						robots[l].setX(500);
+						robots[l].sparkle();
+					}
+				}
 		// 8. try different races with different amounts of robots.
 
 		// 9. make the robots race around a circular track.
+
 	}
 
 }
